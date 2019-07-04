a5a88167bc62a2b642dd1ce991fdff387c4de909
Suvanjan Mukherjee <suvanjan@google.com>
2019-07-04 15:01:58 -0400

    Generate a tag when there's a new bazel release (#412)
    * Generate a tag when there's a new bazel release
    
    Also add a schedule to release immediately when there's a new bazel
    release. For all other updates, make it weekly.
    
    * Change order

--- layers/ubuntu1604/bazel/extract_version.sh
+++ layers/ubuntu1604/bazel/extract_version.sh
@@ -13,19 +13,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This script accepts a tarball with the bazel binary available at
-# "usr/local/lib/bazel/bin" and produces a YAML file with the bazel version
+# This script accepts the following arguments:
+# 1. The bazel version pointed to by :revisions.bzl.
+# 2. Path to a tarball with the latest bazel binary available at
+#    "usr/local/lib/bazel/bin".
+# 3. Path to the output YAML file to generate.
+# The script produces a YAML file with the bazel version
 # which looks as follows:
 # packages:
 # - name: bazel
 #   version: "<version string>"
 # where <version string> is the Bazel version, e.g., 0.27.1
+# Also, if the bazel version in revisions.bzl differs from the bazel binary
+# in the given tarball, the script adds the following section in the YAML:
+# tags:
+# - new_bazel_release
 
 set -e
 
-input_tar=$1
-output_yaml=$2
+current_version=$1
+input_tar=$2
+output_yaml=$3
 
+echo "Current Bazel version: " $current_version
 echo "Input tar: " $input_tar
 echo "Output YAML: " $output_yaml
 
@@ -42,8 +52,18 @@ echo "Running Bazel and extracting the version string"
 version=$(./usr/local/lib/bazel/bin/bazel --output_user_root=$PWD/temp_bazel_user_root version | grep "Build label" | sed -e "s/^\s*Build label:\s*//")
 echo "Extracted Bazel version '$version'"
 
+# Add the package information first because it's always created. This ensures
+# the output file gets replaced with the new contents.
 cat > $output_yaml << EOF
 packages:
-  - name: bazel
-    version: $version
+- name: bazel
+  version: $version
 EOF
+
+if [ "$current_version" != "$version" ]; then
+    echo "New bazel release detected!"
+    cat >> $output_yaml << EOF
+tags:
+- new_bazel_release
+EOF
+fi
